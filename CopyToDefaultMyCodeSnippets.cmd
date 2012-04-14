f98fa9e1d27739d3b15ac034c0a6a7a8cdd314c0
Casey Burns <caseyburns.nz@gmail.com>
2012-04-15 11:49:24 +1200

    created update batch file

--- CopyToDefaultMyCodeSnippets.cmd
+++ CopyToDefaultMyCodeSnippets.cmd
@@ -1,4 +1,12 @@
 @Echo Off
 
-echo .
+SET SnippetsFolder=%USERPROFILE%\My Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets
+
+echo COPYING SNIPPETS TO:
+echo %SnippetsFolder%
+echo.
+
+xcopy *.snippet "%SnippetsFolder%" /Y
+
+echo.
 pause
